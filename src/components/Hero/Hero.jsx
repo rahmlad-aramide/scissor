f7c8ef5cade8000373151e9eecfdecc7dad04727
ABDRAHMAN OLADIMEJI <rahmlad@localhost.localdomain>
2023-07-01 10:15:00 +0100

    update: landing page responsive view done

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,10 +10,10 @@ import cone from '../../assets/images/curved-cone.png';
 const Hero = () => {
   return (
     <section id="url" className="bg-hero-texture bg-cover bg-bottom pt-16">
-      <div className="flex text-3xl md:text-5xl font-bold leading-[70px] md:leading-[96px] max-w-[31ch] mx-auto mt-16">
+      <div className="flex text-3xl md:text-5xl text-center font-bold leading-[50px] md:leading-[96px] max-w-[31ch] mx-auto mt-16">
         Optimize Your Online Experience with Our
       </div>
-      <div className="text-3xl md:text-5xl font-bold leading-[70px] md:leading-[96px] max-w-[26ch] mx-auto">
+      <div className="text-3xl md:text-5xl text-center font-bold leading-[50px] md:leading-[96px] max-w-[26ch] mx-auto">
         Advanced
         <span className="text-primary relative">
           {'  '}
@@ -21,13 +21,13 @@ const Hero = () => {
           <img
             src={underline}
             alt="-"
-            className="absolute -bottom-4 right-24"
+            className="absolute -bottom-4 right-3 md:right-24"
           />{' '}
         </span>{' '}
         Solution
       </div>
       <div className="flex mt-4">
-        <div className="text-lg font-medium text-center max-w-[63ch] mx-auto">
+        <div className="text-lg font-medium text-center w-[90%] max-w-[63ch] mx-auto">
           Personalize your shortened URLs to align with your brand identity.
           Utilize custom slugs, branded links, and domain customization options
           to reinforce your brand presence and enhance user engagement.
@@ -41,19 +41,19 @@ const Hero = () => {
           <HashLink
             to="/#features"
             smooth
-            className="text-[#0065FE] font-semibold ml-14"
+            className="text-[#0065FE] font-semibold ml-4 md:ml-14"
           >
             Learn More
           </HashLink>
         </div>
       </div>
-      <div className="flex mb-16 mt-10">
+      <div className="flex mb-10 md:mb-16 mt-10">
         <div className="relative mx-auto w-fit backdrop-blur-[106px] rounded-3xl">
           <div className="absolute -left-32 -top-4">
             <img src={cone} alt="cone" />
           </div>
           <div className="bg-[#fefefe]/40 backdrop-blur-lg-[106px] rounded-3xl">
-            <div className="bg-[#fefefe]/10 backdrop-blur-[106px] rounded-3xl border-[0.5px] border-primary">
+            <div className="bg-[#fefefe]/10 backdrop-blur-[106px] rounded-3xl border-[0.5px] border-primary w-[90%] mx-auto">
               <div className="flex items-center mx-16 mt-8">
                 <div>
                   <img src={chainLink} alt="chain link" />
@@ -75,7 +75,7 @@ const Hero = () => {
           </div>
         </div>
       </div>
-      <div className="h-24 w-full bg-hero-pattern bg-bottom bg-cover"></div>
+      <div className="h-6 md:h-24 w-full bg-hero-pattern bg-bottom bg-cover"></div>
     </section>
   );
 };
