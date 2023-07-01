34ab16db8f7cf638f076ed158e048fbce7422d21
ABDRAHMAN OLADIMEJI <rahmlad@localhost.localdomain>
2023-07-01 13:29:30 +0100

    update: added dashboard nav

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -7,77 +7,64 @@ import { Button } from '../../components';
 import open from '../../assets/icons/hamburger.svg';
 const Dashboard = () => {
   const { user } = useContext(UserContext);
+  const [active, setActive] = useState(false);
   console.log(user);
   const navRef = useRef();
+  const togglerRef = useRef();
   const [activeLink, setActiveLink] = useState(0);
   const handleActiveLink = (linkNum) => {
     setActiveLink(linkNum);
   };
   const showMenu = () => {
-    navRef.current.classList.toggle('translate-x-[100%]');
+    setActive(!active);
+    navRef.current.classList.toggle('-translate-x-[100%]');
   };
+
   return (
     <section>
       <ToastContainer />
       <div className="pt-16">
         <div className="z-20 flex md:hidden">
-          <button
+          {/* <button
             onClick={showMenu}
-            className="absolute top-0 right-2 flex h-12 items-center justify-center p-3 text-secondary transition active:scale-90 md:hidden"
+            ref={togglerRef}
+            className="absolute top-16 z-50 -translate-x-[100%] right-[70%] flex h-12 items-center justify-center p-3 text-secondary transition active:scale-90 md:hidden"
           >
             <img src={open} alt="Open Nav" />
-          </button>
+          </button> */}
           <div
             ref={navRef}
-            className={`fixed top-0 right-0 my-auto flex h-screen w-[70%] translate-x-[100%] flex-col items-center bg-white text-primary transition md:hidden`}
+            className={`fixed top-0 left-0 my-auto flex h-screen w-[70%] -translate-x-[100%] flex-col items-center bg-gray-200 text-primary transition md:hidden`}
           >
-            <div
-              className="mb-4 mt-[50%] flex justify-center"
+            <button
               onClick={showMenu}
+              ref={togglerRef}
+              className={
+                active
+                  ? `bg-white rounded-full absolute top-16 z-50 -translate-x-[90%] -right-20 flex h-12 w-12 items-center justify-center text-secondary transition active:scale-90 md:hidden`
+                  : `bg-white rounded-full absolute top-16 z-50 -translate-x-[90%] -right-[5.5rem] flex h-12 items-center justify-center p-3 text-secondary transition active:scale-90 md:hidden rotate-180`
+              }
             >
-              <HashLink
-                to="/#"
-                smooth
-                className="px-4 decoration-2 underline-offset-4 hover:underline"
+              <svg
+                stroke="currentColor"
+                fill="currentColor"
+                stroke-width="0"
+                viewBox="0 0 24 24"
+                role="graphics-document"
+                height="2em"
+                width="2em"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                My Urls
-              </HashLink>
-            </div>
-            <div className="my-4 flex justify-center" onClick={showMenu}>
-              <HashLink
-                to="/#features"
-                smooth
-                className="px-4 decoration-2 underline-offset-4 hover:underline"
-              >
-                Features
-              </HashLink>
-            </div>
-            <div className="my-4 flex justify-center" onClick={showMenu}>
-              <HashLink
-                to="/#pricing"
-                smooth
-                className="px-4 decoration-2 underline-offset-4 hover:underline"
-              >
-                Pricing
-              </HashLink>
-            </div>
-            <div className="my-4 flex justify-center" onClick={showMenu}>
-              <HashLink
-                to="/#analytics"
-                smooth
-                className="px-4 decoration-2 underline-offset-4 hover:underline"
-              >
-                Analytics
-              </HashLink>
-            </div>
-            <div className="my-4 flex justify-center" onClick={showMenu}>
-              <HashLink
-                to="/#faqs"
-                smooth
-                className="px-4 decoration-2 underline-offset-4 hover:underline"
-              >
-                FAQs
-              </HashLink>
+                <path fill="none" d="M0 0h24v24H0z"></path>
+                <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path>
+              </svg>
+            </button>
+            <div className="flex mt-20">
+              <div className="text-black">
+                <div>
+                  Test
+                </div>
+              </div>
             </div>
             <button
               onClick={showMenu}
