0bcdc4134f2038dbaeafd62bd90d378bbfac1d56
ABDRAHMAN OLADIMEJI <rahmlad@localhost.localdomain>
2023-07-01 09:52:46 +0100

    update: fixed height on auth pages

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -91,11 +91,11 @@ const SignUp = () => {
 
   return (
     <>
-      <section className="h-screen bg-white">
+      <section className="h-full md:h-screen bg-white">
         <ToastContainer />
-        <div className="flex h-full md:h-screen w-full flex-col items-center justify-start md:flex-row md:justify-center">
-          <div className="order-1 flex h-fit w-full items-center justify-center pt-16 md:order-2 md:h-full md:w-[45%]">
-            <div className="my-auto h-full w-[90%] overflow-y-auto rounded-xl py-5">
+        <div className="flex h-full md:min-h-screen w-full flex-col items-center justify-start md:flex-row md:justify-center">
+          <div className="order-1 flex h-fit w-full items-center justify-center pt-0 md:pt-16 md:order-2 md:h-full md:w-[45%]">
+            <div className="my-auto h-full w-[90%] overflow-y-auto rounded-xl py-10">
               <div className="h-full w-full overflow-y-auto">
                 <form
                   onSubmit={handleSubmit}
